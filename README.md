5175e590fe922a248aa883342a07fa6b36a79dfa
berg <towong@qpp.com>
2023-10-26 11:20:38 +0800

    feat-directory: 添加数据结构与算法类目

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # 重温c
 
+## help
+Windows环境中：
+gcc hello.c 编译hello.c源文件并生成hello.exe可执行文件
+
+windows commond运行可执行文件直接打入文件名(hello)回车直接运行
 
 ### target:write an operate system kernel
---
+++ 数据结构与算法/数据结构/Array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>  //io头文件用于调用 printf函数
+#include <stdlib.h>   //lib头文件用于调用system函数
+#include <math.h>
+
+//主函数
+int main()
+{
+ 
+  system("pause");  //暂停运行，否则直接运行完程序会直接关闭窗口（vscode需要，其他编译器看情况） 
+}
