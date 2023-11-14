0748513ba3a3e2b379217929d32e1fcb45bfbd5e
berg <towong@qpp.com>
2023-11-14 09:16:58 +0800

    feat-data-structures: array.c

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ## help
 Windows环境中：
-gcc hello.c 编译hello.c源文件并生成hello.exe可执行文件
+gcc [源文件名] -o [目标文件名] 
+gcc hello.c -o hello 编译hello.c源文件并生成hello.exe可执行文件
 
 windows commond运行可执行文件直接打入文件名(hello)回车直接运行
 
--- 数据结构与算法/数据结构/Array.c
+++ 数据结构与算法/数据结构/Array.c
@@ -1,10 +1,48 @@
-#include <stdio.h>  //io头文件用于调用 printf函数
-#include <stdlib.h>   //lib头文件用于调用system函数
-#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-//主函数
+/*
+  Describe：数据操作
+  Author： maGod
+*/
+
 int main()
 {
- 
-  system("pause");  //暂停运行，否则直接运行完程序会直接关闭窗口（vscode需要，其他编译器看情况） 
+  // 初始化数组
+  int nums[5] = {1, 2, 3, 4, 5};
+
+  printf("the random number is: %d \n", randomAccess(nums, 5));
+
+  insert(nums, 5, 2, 6);
+  printf("let 6 insert to the index 2 of nums:");
+  printArray(nums, 5);
+
+  system("pause");
+}
+
+// 将元素插入数组中的指定位置
+void insert(int *nums, int size, int index, int data)
+{
+  // 把索引 index 以及之后的所有元素向后移动一位
+    for (int i = size - 1; i > index; i--) {
+        nums[i] = nums[i - 1];
+    }
+    // 将 num 赋给 index 处元素
+    nums[index] = data;
+}
+
+// 随机访问元素
+int randomAccess(int *nums, int size)
+{
+  int randomIndex = rand() % size;
+  return nums[randomIndex];
+}
+
+// 打印数组
+void printArray(int *nums, int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    printf("%d ", nums[i]);
+  }
 }
