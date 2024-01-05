c61b795631634b2b063eecf224110ee5ebcfe637
berg <towong@qpp.com>
2024-01-05 09:53:52 +0800

    feat-dataStruct fill Link

--- 数据结构与算法/数据结构/Link.c
+++ 数据结构与算法/数据结构/Link.c
@@ -23,8 +23,9 @@ Link *newLink(int value)
     return node;
 }
 
-//构建一个1-5的链表
-Link *initLinkList(){
+// 构建一个1-5的链表
+Link *initLinkList()
+{
     Link *link1 = newLink(1);
     Link *link2 = newLink(2);
     Link *link3 = newLink(3);
@@ -42,20 +43,73 @@ Link *initLinkList(){
 /*
 打印链表
 */
-void printLink(Link *head){
+void printLink(Link *head)
+{
     while (head != NULL)
     {
-        printf("%d ",head->value);
+        printf("%d ", head->value);
         head = head->next;
     }
-    
+}
+
+/*
+插入节点
+*/
+void insert(Link *head, Link *p, int position)
+{
+    for (size_t i = 0; i <= position; i++)
+    {
+        if (i == position)
+        {
+            Link *temp = head -> next;
+            p -> next = temp;
+            head -> next = p;
+            break;
+        }
+        else
+        {
+            head = head->next;
+        }
+    }
+}
+
+/*
+删除节点
+*/
+void delete(Link *head, int position)
+{
+    for (size_t i = 0; i <= position; i++)
+    {
+        if (i == position)
+        {
+            Link *needDelete = head -> next;
+            Link *temp = needDelete -> next;
+            head -> next = temp;
+            free(needDelete);
+            break;
+        }
+        else
+        {
+            head = head->next;
+        }
+    }
 }
 
 int main()
 {
     Link *head = initLinkList();
-
     printLink(head);
-    
+    printf("\n");
+
+    Link *link8 = newLink(8);
+
+    insert(head, link8, 2);
+    printLink(head);
+    printf("\n");
+
+    delete(head, 2);
+    printLink(head);
+    printf("\n");
+
     system("pause");
 }
